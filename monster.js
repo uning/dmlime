274a055d96d5204fa722fefbc8733e5e1babe812
hotel@ldev <hotel@ldev>
2012-02-10 19:31:28 +0800

    怪物技能进行中

---
+++ monster.js
@@ -0,0 +1,281 @@
+goog.provide('dm.Monster');
+goog.require('dm.conf.MS');
+
+dm.Monster = function(turn, p, game, mon_id){
+	this.game = game;
+	this.board = game.board;
+	this.conf = dm.conf.MS;
+	this.genAttribute(turn, p, mon_id);
+}
+
+dm.Monster.prototype.genAttribute = function(turn, p, mon_id){
+	//基础属性
+	this.p = p;//gem
+	this.attack = 1 + Math.floor(turn/40); 
+	this.hp = Math.floor(turn/30)+4;
+	this.def = Math.floor(turn/40)+1;
+	this.aliveturn = 0;
+
+	var mon_arr = this.game.data['specialMon'];
+	//特殊怪物?
+	this.id = mon_id;
+	if(!this.id){
+		//if(this.game.user.lvl > 4 && Math.random()*100 > 95){
+		if(Math.random()*100 > 70){
+			//
+			//var index = Math.round(Math.random()*(mon_arr.length-1));
+			//this.id = this.game.data.specialMon.splice(index, 1);
+
+			//test
+			this.id = 2;
+		}else{
+			this.id = 0;
+		}
+	}
+	//附加属性
+	var id = this.id;
+	var config = this.conf[id];
+	//
+	this.attack += parseInt(config.attack);
+	this.def += parseInt(config.defense);
+	this.hp += parseInt(config.hp);
+	this.skill = config.skill;
+	this.bounce = config['bounce'];
+	//
+	this.def_left = this.def;
+	this.hp_left = this.hp;
+	//状态参数
+	this.delay = config.delay;
+	this.poison = 0; //受毒伤害
+	this.poison_start = 1; //受毒伤害
+	this.stone = 0; //石化伤害
+
+	//显示相关，标签
+	var name = config.name;
+	var tips = config.tips;
+	var size = p.getSize(), 
+	h=size.height, 
+	w=size.width;
+	this.attlabel = new lime.Label().setFontFamily('Trebuchet MS').setFontColor('#000').setFontSize(30).setAnchorPoint(1, 0.5).setText(this.attack);
+	this.hplabel = new lime.Label().setFontFamily('Trebuchet MS').setFontColor('#f00').setFontSize(30).setAnchorPoint(1, 0.5).setText(this.hp_left);
+	this.deflabel = new lime.Label().setFontFamily('Trebuchet MS').setFontColor('#00f').setFontSize(30).setAnchorPoint(1, 0.5).setText(this.def);
+	p.appendChild(this.attlabel.setPosition(w*0.4, -h/4));
+	p.appendChild(this.hplabel.setPosition(w*0.4, 0));
+	p.appendChild(this.deflabel.setPosition(w*0.4, h/4));
+
+}
+
+/**
+ *当怪物被杀死的时候，得到相应奖励
+*/
+dm.Monster.prototype.addBounce = function(){
+	var data = this.game.data;
+	for(var i in this.bounce){
+		this.game.updateData(i, parseInt(this.bounce[i]), 'add');
+	}
+}
+
+
+/**
+ * 计算存活轮数
+*/
+dm.Monster.prototype.incAliveTurn= function(){
+	this.aliveturn++;
+}
+
+
+/**
+ * 设定怪物存在的轮数
+*/
+dm.Monster.prototype.suicide = function(turn){
+	if(this.aliveturn >= turn){
+		this.p.keep = false;
+		var nobounce = false;
+		this.onDeath(nobounce);
+	}
+}
+
+
+/**
+ *怪物死亡后产生作用,复原一些怪物改变的参数
+ * param@ bounce -- true 有奖励 ， false 无奖励
+ */
+dm.Monster.prototype.onDeath = function(bounce){
+	//使怪物可以产生
+	if(this.id){
+		this.game.data.specialMon.push(this.id);  
+		//怪物技能影响复原
+		this.endSkill();
+		//获得奖励
+		if(bounce){
+			this.addBounce();
+		}
+	}
+}
+/** 
+ * 回合开始的动作
+ */
+ dm.Monster.prototype.startTurn = function(monster){
+	if(monster){
+		if(monster.delay == 0)
+			monster.useSkill();
+	}else{
+		if(this.delay == 0)
+			this.useSkill();
+	}
+
+ }
+
+/**
+ * 每轮结束后执行的动作
+ */
+dm.Monster.prototype.endTurn = function(monster){
+	if(monster){
+		monster.incAliveTurn();
+		if(monster.delay == 1){
+			monster.useSkill();
+		}
+	}else{
+		this.incAliveTurn();
+		if(this.delay == 1){
+			this.useSkill();
+		}
+	}
+}
+
+/**
+ * 怪物使用相应的技能
+ */
+dm.Monster.prototype.useSkill = function(){
+	switch(this.skill){
+		case '1':{
+			this.suicide(5);
+			break;
+		}
+		case '2':{
+			this.game.updateData('canCD', 0);
+			break;
+		}
+		case '3':{
+			break;
+		}
+		case '4':{
+			break;
+		}
+		case '5':{
+			break;
+		}
+		case '6':{
+			break;
+		}
+		case '7':{
+			break;
+		}
+		case '8':{
+			break;
+		}
+		case '9':{
+			break;
+		}
+		case '10':{
+			break;
+		}
+		case '11':{
+			break;
+		}
+		case '12':{
+			break;
+		}
+		case '13':{
+			break;
+		}
+		case '14':{
+			break;
+		}
+		case '15':{
+			break;
+		}
+		case '16':{
+			break;
+		}
+		case '17':{
+			break;
+		}
+		case '18':{
+			break;
+		}
+		case '19':{
+			break;
+		}
+		case '20':{
+			break;
+		}
+	}
+}
+
+
+/**
+ * 怪物死亡后复原技能影响
+ */
+dm.Monster.prototype.endSkill = function(){
+	switch(this.skill){
+		case '2':{
+			this.game.updateData('canCD', 1);
+			break;
+		}
+		case '3':{
+			break;
+		}
+		case '4':{
+			break;
+		}
+		case '5':{
+			break;
+		}
+		case '6':{
+			break;
+		}
+		case '7':{
+			break;
+		}
+		case '8':{
+			break;
+		}
+		case '9':{
+			break;
+		}
+		case '10':{
+			break;
+		}
+		case '11':{
+			break;
+		}
+		case '12':{
+			break;
+		}
+		case '13':{
+			break;
+		}
+		case '14':{
+			break;
+		}
+		case '15':{
+			break;
+		}
+		case '16':{
+			break;
+		}
+		case '17':{
+			break;
+		}
+		case '18':{
+			break;
+		}
+		case '19':{
+			break;
+		}
+		case '20':{
+			break;
+		}
+	}
+}
---
+++ skill.js
@@ -0,0 +1,339 @@
+goog.provide('dm.Skill');
+
+dm.Skill = function(game){
+	//this.skills = dm.conf.SK;
+	this.game = game;
+	this.board = game.board;
+	this.user = game.user;
+	this.sk_conf = dm.conf.SK;
+}
+
+/*
+ * 使用技能，有buff效果的，设置持续轮数，对于非即时产生效果的技能，在回合中才发生作用
+ */
+dm.Skill.prototype.use = function(id){
+	//使用技能
+	if(this.game.skillCD[id] > 0){
+		alert('技能冷却中, 还有'+this.game.skillCD[id]+'轮');
+	}else{
+		this.game.skillCD[id] = 3
+		this.game.buff[id] = parseInt(this.sk_conf['sk'+id]['turn']); //持续时间
+		if(parseInt(this.sk_conf['sk'+id]['delay']) == 0){ //立即施展技能
+			this.action(id);
+		}
+	}
+}
+
+
+/*
+ * 技能产生的作用效果
+ */
+dm.Skill.prototype.action = function(id, param){ //各个技能的作用效果
+	switch(id){
+		case '1':{ //加血
+			this.game.updateData('hp', Math.min(Math.round(this.game.data.hp*1.1), this.user.fp.a6));
+		}
+		break;
+		case '2':{ //
+			this.game.updateData('hp', Math.min(Math.round(this.game.data.hp*1.2), this.user.fp.a6));
+		}
+		break;
+		case '3':{ //法力转换为生命
+			this.game.updateData('hp', Math.min(this.game.data.hp + this.game.data.mana, this.user.fp.a6));
+			this.game.updateData('mana', 0);
+		}
+		break;
+		case '4':{
+			//每把武器带来的额外攻击力
+			this.game.updateData('attack_ratio', 5);
+		}
+		break;
+		case '5':{
+			//造成法力值的伤害
+			this.game.updateData('attack_magic', this.game.data['mana']);
+			this.game.updateData('mana', 0);
+			this.board.findMonster(this.magicAttack, this.game);
+		}
+		break;
+		case '6':{
+			//敌人受到伤害*2
+			this.game.updateData('dmgRatio', 2);
+		}
+		break;
+		case '7':{//防御力提升100%,属于额外护甲，受到伤害减少，或者持续3轮
+			this.game.updateData('enhenceDef', 100);
+			if(this.game.buff[7] == 3){//只在第一次加额外防御，额外防御承受攻击而减少，3回合后清零
+				this.game.updateData('def_extra', this.game.data.enhenceDef/100 * this.game.user.fp.a3);
+			}
+		}
+		break;
+		case '8':{//绝对防御 
+			this.game.updateData('noDmg', 1);
+		}
+		break;
+		case '9':{//虚弱无力 降低敌人伤害,每轮效果递减10%
+			this.game.updateData('reduceDmg', 10*this.game.buff[9]);
+		}
+		break;
+		case '10':{//老化，敌人的生命值降低20%
+			this.reduceHp(20)
+		}
+		break;
+		case '11':{
+			this.game.updateData('extAvoid', 20);
+		}
+		break;
+		case '12':{//火球，随机消除3*3范围内的物体
+			this.wipeBlock();
+		}
+		break;
+		case '13':{//搜集某类图标
+			this.collect('hp', 'exp');
+		}
+		break;
+		case '14':{//重排列所有图标
+			this.wipeAll();
+		}
+		break;
+		case '15':{//新图标全部变成金币
+			this.board.genType = 4; //新产生的图标类型
+		}
+		break;
+		case '16':{//金币获得双倍效果
+			this.game.updateData('doublegain', 1);
+		}
+		break;
+		case '17':{ //本轮受到伤害死亡，可以复活
+			this.game.updateData('revive', 1);
+		}
+		break;
+		case '18':{ //敌人无法使用特殊技能
+			this.game.updateData('disableSkill', 1);
+		}
+		break;
+	}
+}
+
+dm.Skill.prototype.actionEnd = function(id, param){ //技能作用完毕的清理工作
+	var buff = this.game.buff;
+	switch(id){
+		case '4':{
+			this.game.updateData('attack_ratio', 0);
+		}
+		break;
+		case '6':{
+			this.game.updateData('dmgRatio', 1);
+		}
+		break;
+		case '7':{
+			this.game.updateData('def_extra', 0);
+		}
+		break;
+		case '8':{//绝对防御 
+			this.game.updateData('noDmg', 0);
+		}
+		break;
+		case '9':{//虚弱无力 降低敌人伤害
+			this.game.updateData('reduceDmg', 0);
+		}
+		break;
+		case '11':{
+			this.game.updateData('extAvoid', 0);
+		}
+		break;
+		case '15':{//新图标全部变成金币
+			this.board.genType = -1; //新产生的图标类型
+		}
+		break;
+		case '16':{//金币获得双倍效果
+			this.game.updateData('doublegain', 0);
+		}
+		break;
+		case '17':{ //本轮受到伤害死亡，可以复活
+			this.game.updateData('revive', 0);
+		}
+		break;
+		case '18':{ //敌人无法使用特殊技能
+			this.game.updateData('disableSkill', 0);
+		}
+		break;
+	}
+}
+
+/**
+ * 减少敌人HP
+ */
+dm.Skill.prototype.reduceHp = function(ratio){
+  this.board.findMonster(function(g, context, ratio){
+	  g.monster.hp_left = Math.floor(g.monster.hp_left*(100 - ratio)/100);
+	  if(g.monster.hp_left > 0){
+		  g.keep = true;
+	  }else{
+		  g.keep = false;
+		  context.updateData('exp', 1, 'add');
+	  }
+	  g.monster.hplabel.setText(g.monster.hp_left);
+  }, this.game, ratio);
+}
+
+
+  /**
+   * 魔法攻击，造成剩余魔法值等量的伤害
+   */
+  dm.Skill.prototype.magicAttack = function(g , game){
+	  var i;
+	  var dmg = game.data.attack_magic;
+	  g.monster.hp_left = g.monster.def_left + g.monster.hp_left - dmg;
+	  if(g.monster.hp_left <= 0){
+		  g.keep = false;
+		  game.updateData('exp', 1, 'add');
+	  }else{
+		  g.monster.hplabel.setText(g.monster.hp_left);
+		  g.keep = true;
+	  }
+  } 
+
+
+ /**
+  * 收集某类图标，产生作用获得其相关的奖励
+  */
+   dm.Skill.prototype.gainGem = function(gem){
+	   var fp = this.game.user.fp
+	   ,sp = this.game.user.sp
+	   ,data = this.game.data
+	   ,p_type
+	   switch(gem.type){
+		   case 'sword':{
+			   this.game.updateData('attack_addtion', fp.a2, 'add');
+		   }
+		   break;
+		   case 'monster':{
+			   this.game.updateData('exp', fp.a17, 'add');
+		   }
+		   break;
+		   case 'hp':{
+			   this.game.updateData('hp', fp.a9, 'add');
+		   }
+		   break;
+		   case 'gold':{
+			   this.game.updateData('gold', fp.a13, 'add');
+		   }
+		   break;
+		   case 'mana':{
+			   this.game.updateData('mana', fp.a21, 'add');
+		   }
+		   break;
+	   }
+   }
+
+ /**
+  * 重新排列所有的图标
+  */
+   dm.Skill.prototype.reArrange = function(s){//, keep){
+	   //先删除显示层元素
+	   this.board.clearGem(true);
+	   var c, r, copy;
+	   for (c = 0; c < this.board.cols; c++) {
+		   if ( !this.board.gems[c] ) 
+			   this.board.gems[c] = [];
+		   i = 0;
+		   for (r = this.board.gems[c].length; r < this.board.rows; r++) {
+			   copy = s.splice(Math.round(Math.random()*(s.length-1)), 1);//随机选择一个gem
+			   i++;
+			   var gem = copy[0];
+			   gem.r = r;
+			   gem.c = c;
+			   gem.setPosition((c + .5) * this.board.GAP, (-i + .5) * this.board.GAP);
+			   //gem.setSize(this.GAP, this.GAP);
+			   this.board.gems[c].push(gem);
+			   this.board.layers[c].appendChild(gem);
+		   }
+	   }
+	   this.board.moveGems();
+   }
+
+
+  /**
+   * 找到要改变的gems,
+   * param: mod -- all 全部选中 ; block，选3X3的块，并且消除
+   */
+   dm.Skill.prototype.findGem = function(mod){//, keep){
+	   var s = [];
+	   var g;
+	   if(mod == 'All'){
+		   var c, r;
+		   for (c = 0; c < this.board.cols; c++) {
+			   for (r = 0; r < this.board.rows; r++) {
+				   g = this.board.gems[c][r];
+				   g.keep = false;
+				   s.push(g);
+			   }
+		   }
+	   }else{//取3*3的一个方块内的元素
+		   var anchor = [
+			   Math.round(1 + Math.random()*(this.board.cols - 3)),
+			   Math.round(1 + Math.random()*(this.board.rows - 3))
+		   ];
+
+
+		   for(c = -1; c < 2; c++){
+			   for(r = -1; r < 2; r++){
+				   g = this.board.gems[anchor[0] + c][anchor[1] + r];
+				   g.keep = false;
+				   s.push(g);
+			   }
+		   }
+	   }
+	   return s;
+   }
+
+   /**
+	* 找到某类gems，对每个对象调用回调函数
+	*/
+   dm.Skill.prototype.findType = function(type, func, param, context){
+	   var c, i, r, g, arr = [];
+	   for (c = 0; c < this.board.cols; c++) {
+		   for (r = 0; r < this.board.rows; r++) {
+			   if(this.board.gems[c][r].type == type){
+				   func(this.board.gems[c][r], param, context);
+			   }
+		   }
+	   }
+	   this.game.board.clearGem();
+   }
+
+
+  /**
+   * 技能：将当前的血瓶转换为经验
+   * param@ type:搜集图标的类型， value：搜集图标增加的值的类型
+   */
+   dm.Skill.prototype.collect = function(type, value){
+	   game = this.game;
+	   this.findType(type,function(g, value, game){
+		   g.keep = false;
+		   game.updateData(value, 1, 'add');
+	   },value, game)
+   }
+
+
+ /**
+  * 技能：随机消除图中3*3的格子
+  */
+  dm.Skill.prototype.wipeBlock = function(){
+	  var s = this.findGem('block', false);
+	  for(var i in s){
+		  this.gainGem(s[i]);
+	  }
+	  this.game.board.clearGem();
+  }
+
+
+ /**
+  * 技能：替换所有的图标，全部刷新
+  */
+  dm.Skill.prototype.wipeAll = function(){
+	  var s = this.findGem('All', true);
+	  this.reArrange(s);
+  }
+
+
